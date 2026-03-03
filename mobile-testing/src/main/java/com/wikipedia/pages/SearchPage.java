3453a1a558858d49ce1ce86ae84be77a96bc5e52
Khushbakht Khan <132307035+khushbakht-khan0@users.noreply.github.com>
2026-03-03 13:12:37 +0500

    Add imports for WebDriverWait and By

--- mobile-testing/src/main/java/com/wikipedia/pages/SearchPage.java
+++ mobile-testing/src/main/java/com/wikipedia/pages/SearchPage.java
@@ -5,6 +5,9 @@ import io.appium.java_client.pagefactory.AndroidFindBy;
 import io.appium.java_client.pagefactory.AppiumFieldDecorator;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import java.util.List;
 
 public class SearchPage {
